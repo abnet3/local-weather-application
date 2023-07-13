3b0533d3ba1c72de4fee99daae8b5d850f20a5da
abnet3 <abnethussien6@gmail.com>
2023-07-13 13:45:48 +0300

    Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { MaterialModule } from './material.module'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
